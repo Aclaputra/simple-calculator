d12a931fee30be5c184d3c089d35f6f6b8e078a3
M Acla A Putra <67270918+Aclaputra@users.noreply.github.com>
2021-02-14 20:15:15 +0700

    first commit

---
+++ calculator.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator PHP</title>
+</head>
+<body>
+    <form action="calculator.php" method="post">
+        First Num: <br><input type="number" step="0.001" name="num1" id="num1"> <br>
+        OP: <br><input type="text" name="op" id="op"> <br>
+        Second Num: <br><input type="number" name="num2" id="num2"> <br>
+        <input type="submit">
+    </form>
+
+    <?php 
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $op = $_POST["op"];
+
+        if($op == "+"){
+            echo $num1 + $num2;
+        } elseif($op == "-"){
+            echo $num1 - $num2;
+        } elseif($op == "/"){
+            echo $num1 / $num2;
+        } elseif($op == "*"){
+            echo $num1 * $num2;
+        } else {
+            echo "Invalid Operator";
+        }
+    ?>
+</body>
+</html>
